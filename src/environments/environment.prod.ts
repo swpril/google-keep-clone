857c614dd5b75a10706329d2de11f6abb1657c13
Swpril Ahuja <ahuja.s0007@gmail.com>
2022-03-26 20:05:31 +0530

    Update env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: `your api key`,
+    authDomain: `your auth domain`,
+    projectId: `your project id`,
+    storageBucket: `your storage bucket`,
+    messagingSenderId: `your messaging sender id`,
+    appId: `your app id`
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: `your api key`,
+    authDomain: `your auth domain`,
+    projectId: `your project id`,
+    storageBucket: `your storage bucket`,
+    messagingSenderId: `your messaging sender id`,
+    appId: `your app id`
+  }
 };
 
 /*
